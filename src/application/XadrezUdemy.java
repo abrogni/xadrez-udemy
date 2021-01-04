6f1bbcc6ee41aae176f4236d3588c5a32106d252
Antonio Jose Brogni <antoniojosebrogni@gmail.com>
2021-01-04 12:05:34 -0300

    Chess Layer and Printing the Board

--- src/application/XadrezUdemy.java
+++ src/application/XadrezUdemy.java
@@ -1,12 +1,14 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessMatch;
 
 public class XadrezUdemy {
 
     public static void main(String[] args) {
         
-        Board board = new Board(8, 8);
+        ChessMatch chessmMatch = new ChessMatch();
+        UI.printBoard(chessmMatch.getPieces());
         
     }
     
